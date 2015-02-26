145269d783350a3955ff093543856b930f15eb1c
Edwin Estrada <edwin@thinkpixbit.com>
2015-02-26 15:22:40 -0500

    Filename for prism JS and CSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,6 +59,14 @@
 
         $(".scroll-down").arctic_scroll();
 
+        $('.filename').each(function(){
+          var p_tag = $(this).parent();
+          var filename = $(this);
+
+          filename.insertAfter(p_tag);
+          p_tag.remove();
+        });
+
     });
 
     // smartresize
